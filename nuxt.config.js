28d6f722c6384c0a3fc25d5dde4f650fbeef4f0d
Alptuğ <alptugidin@gmail.com>
2022-03-15 03:29:44 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,109 +1,98 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'KDramaLike - Search and find your favorite Koran Dramas',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'KDramaLike - Search and find your favorite Koran Dramas',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'kdramalike.com find and list korean dramas similar to your favourite.'
+            },
+            {name: 'format-detection', content: 'telephone=no'},
+            {
+                name: 'keywords',
+                content: 'kdramalike, kdramas like, korean dramas like, kdramas to similar, korean dramas  to similar'
+            },
+
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/templates/favicoT2.ico'}
+        ],
+        script: [
+            {type: "text/javascript", src: "/scripts/metric.js"}
+        ]
     },
-    meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'kdramalike.com find and list korean dramas similar to your favourite.'
-      },
-      {name: 'format-detection', content: 'telephone=no'},
-      {
-        name: 'keywords',
-        content: 'kdramalike, kdramas like, korean dramas like, kdramas to similar, korean dramas  to similar'
-      },
 
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        `@/static/styles.css`,
+        `@/node_modules/bulma-modal-fx/dist/css/modal-fx.min.css`
     ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/templates/favicoT2.ico'}
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        // {src: `@/node_modules/bulma-modal-fx/dist/js/modal-fx.min.js`, mode: `client`}
     ],
-    script:[
-      {type: "text/javascript", src: "/scripts/metric.js"}
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    `@/static/styles.css`,
-    `@/node_modules/bulma-modal-fx/dist/css/modal-fx.min.css`
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    // {src: `@/node_modules/bulma-modal-fx/dist/js/modal-fx.min.js`, mode: `client`}
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/google-fonts',
-    // '@nuxtjs/fontawesome'
-  ],
-
-  // fontawesome: {
-  //   icons: {
-  //     solid: true,
-  //     brands: true
-  //   }
-  // },
-
-  googleFonts: {
-    families: {
-      Ubuntu: [300]
-    }
-  },
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/buefy
-    ['nuxt-buefy', {materialDesignIcons: false}],
-    // '@nuxtjs/proxy'
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        '@nuxtjs/google-fonts',
+    ],
 
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extractCSS: true
-  },
+    googleFonts: {
+        families: {
+            Ubuntu: [300]
+        }
+    },
 
-  serverMiddleware: [
-    // "~/api",
-    "~/api/metrics",
-    "~/api/tags",
-    "~/api/populars",
-    "~/api/series",
-    "~/api/calc",
-    "~/api/tagCalc"
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/buefy
+        ['nuxt-buefy', {materialDesignIcons: false}],
+    ],
 
-  // axios: {
-  //   baseURL: "https://kdramalike.com/api",
-  //   proxyHeaders: false,
-  //   credentials: false
-  // },
-  //
 
-  hooks: {
-    'vue-renderer:ssr:context'(context) {
-      const routePath = JSON.stringify(context.nuxt.routePath);
-      context.nuxt = {serverRendered: true, routePath};
-    }
-  },
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        extractCSS: true
+    },
 
-  //
-  // server:{
-  //   host : '192.168.1.24'
-  // }
-  //
+    serverMiddleware: [
+        "~/api/metrics",
+        "~/api/tags",
+        "~/api/populars",
+        "~/api/series",
+        "~/api/calc",
+        "~/api/tagCalc"
+    ],
 
+
+    hooks: {
+        'vue-renderer:ssr:context'(context) {
+            const routePath = JSON.stringify(context.nuxt.routePath);
+            context.nuxt = {serverRendered: true, routePath};
+        }
+    },
+
+    // privateRuntimeConfig: {
+    //     // dbLocation: process.env.NODE_ENV === "development" ? process.env.DEV_DB : process.env.PROD_DB
+    //     // dbLocation: process.env.DEV_DB
+    // }
+
+    //
+    // server:{
+    //   host : '192.168.1.24'
+    // }
+    //
 
 
 }
