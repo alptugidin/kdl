49103b3c8c558e06b08b36ea94fd12eb2cbe4a82
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-19 05:27:47 +0300

    new footer with tailwindcss

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,18 +1,62 @@
 <template>
   <div>
-    <nav class="absolute bottom-0 h-[36px] w-full bg-[black]">
-
+    <nav class="absolute bottom-0 w-full bg-[#404040]">
+      <div class="container p-1 mx-auto grid d:grid-cols-4 t:grid-cols-4 grid-cols-2">
+        <div class="grid grid-rows-2 items-center">
+          <div>
+            <a href="/"><span id="footer-home" class="d:text-4xl t:text-4xl text-2xl text-[#f5f5f5] font-semibold">KDramaLike</span></a>
+            <span class="text-[#b5b5b5] font-semibold">2022</span>
+          </div>
+          <div>
+            <a href="/#"><span class="text-[#ffc107] font-bold">/</span> <span
+                class="text-[white] hover:underline decoration-[#ffc107] decoration-2">About</span></a>
+            <a href="/#"><span class="text-[#ffc107] font-bold">/</span> <span
+                class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Privacy</span></a>
+            <a href="/#"><span class="text-[#ffc107] font-bold">/</span> <span
+                class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Terms</span></a>
+          </div>
+        </div>
+        <div class="d:col-start-4 d:col-end-4 t:col-start-4 t:col-end-4 ">
+          <div class="grid grid-cols-1 items-center">
+            <div class="text-center">
+              <span class="text-[17px] text-[#dbdbdb]">Most Popular Korean Dramas</span><br>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span><br>
+              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Latest Korean Dramas</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+      </div>
     </nav>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 
 <style>
+
+#footer-home {
+  font-family: "Ubuntu", serif;
+
+}
+
+.dd {
+  color: #ffc107;
+}
 
 </style>
