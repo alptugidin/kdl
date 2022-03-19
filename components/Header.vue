38d8a2f40efc1bc67dbbd2a06e78f0e6305cb2c5
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-19 04:03:33 +0300

    items-center fix for img grid

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <nav class="bg-white border-b border-[#DBDBDB] absolute w-full">
       <div
           class="container d:h-[74px] t:h-[74px] h-[90px] mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center">
-        <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%]">
+        <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%] grid grid-col-1 items-center">
           <a href="/" class="inline-block"><img src="/templates/bannerV3x64.png" alt="" class=""></a>
         </div>
         <div class="t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0">
