37110d960ad6d9526d4979e3835355f821b45d3c
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-19 22:50:13 +0300

    new footer with tailwindcss

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <nav class="absolute bottom-0 w-full bg-[#404040]">
-      <div class="container p-1 mx-auto grid d:grid-cols-4 t:grid-cols-4 grid-cols-2">
-        <div class="grid grid-rows-2 items-center">
-          <div>
-            <a href="/"><span id="footer-home" class="d:text-4xl t:text-4xl text-2xl text-[#f5f5f5] font-semibold">KDramaLike</span></a>
+      <div class="container p-1 mx-auto grid d:grid-cols-4 t:grid-cols-3 grid-cols-1">
+        <div class="items-center d:mb-0 t:mb-0 mb-8">
+          <div class="d:text-left t:text-left text-center">
+            <a href="/"><span id="footer-home" class="d:text-3xl t:text-4xl text-2xl text-[#f5f5f5] font-semibold">KDramaLike</span></a>
             <span class="text-[#b5b5b5] font-semibold">2022</span>
           </div>
-          <div>
+          <div class="d:text-left t:text-left text-center">
             <a href="/#"><span class="text-[#ffc107] font-bold">/</span> <span
                 class="text-[white] hover:underline decoration-[#ffc107] decoration-2">About</span></a>
             <a href="/#"><span class="text-[#ffc107] font-bold">/</span> <span
@@ -16,23 +16,21 @@
                 class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Terms</span></a>
           </div>
         </div>
-        <div class="d:col-start-4 d:col-end-4 t:col-start-4 t:col-end-4 ">
+        <div class="d:col-start-4 d:col-end-4 t:col-start-3 t:col-end-3">
           <div class="grid grid-cols-1 items-center">
-            <div class="text-center">
+            <div class="text-center d:px-0 t:px-0 px-20">
               <span class="text-[17px] text-[#dbdbdb]">Most Popular Korean Dramas</span><br>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">2022</span></a><span class="text-[#ffc107] font-bold"> / </span><br>
-              <a href="#"><span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Latest Korean Dramas</span></a>
+              <div v-for="i in years(2010,2021).reverse()" class="inline-block">
+                <a :href="'/populars/Most-popular-Korean-Dramas-'+i">
+                  <span class="text-[white] hover:underline decoration-[#ffc107] decoration-2">{{ i }}</span>
+                </a>
+                <span class="text-[#ffc107] font-bold">/&nbsp</span><br>
+
+              </div>
+              <br>
+
+              <a href="Latest-Korean-Dramas"><span
+                  class="text-[white] hover:underline decoration-[#ffc107] decoration-2">Latest Korean Dramas</span></a>
             </div>
           </div>
         </div>
@@ -44,7 +42,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      years: (min, max) => [...Array(max - min + 1).keys()].map(i => i + min),
+    }
+  }
+}
 </script>
 
 
