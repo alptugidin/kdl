0275d2090dba2c6d231417ce796954b08277842a
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-20 05:02:56 +0300

    fix

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="bg-white border-b border-[#DBDBDB] absolute w-full">
+    <nav class="bg-white border-b border-[#DBDBDB] w-full">
       <div
           class="container d:h-[74px] t:h-[74px] h-[90px] mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center">
         <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%] grid grid-col-1 items-center">
