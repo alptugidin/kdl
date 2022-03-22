34ef3b960d47279092b782ee3a306c213d69f8b9
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-22 17:39:19 +0300

    fully functional homepage with tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="index-div">
     <div class="container mx-auto">
-
       <div class="pt-10">
         <div class="text-center">
           <span class="ubuntu-font md:text-3xl text-xl font-semibold text-[#4A4A4A]">Latest Korean Dramas</span>
         </div>
-
         <div class="relative container mx-auto carousel">
-          <chevron :direction="directionLeft" :rotate="rotateLeft"/>
-          <chevron :direction="directionRight" :rotate="rotateRight"/>
-          <div
-              class="container mx-auto carousel lg:min-h-[261px] bg-[white] rounded-xl outline outline-[1px] outline-[#dbdbdb] relative flex flex-nowrap lg:overflow-hidden overflow-auto">
+
+          <chevron
+              v-show="c1scroll > 1"
+              id="c1"
+              class="c1"
+              @click.native="chevron"
+              :direction="directionLeft"
+              :rotate="rotateLeft"
+          />
+
+          <chevron
+              v-show="c1scroll !== (c1max - current_width)"
+              id="c2"
+              class="c2"
+              @click.native="chevron"
+              :direction="directionRight" :rotate="rotateRight"
+          />
+
+          <div id="carousel1"
+               class="container mx-auto carousel minh bg-[white] rounded-xl outline outline-[1px] outline-[#dbdbdb] relative flex flex-nowrap lg:overflow-hidden overflow-auto">
             <card
                 class="p-1"
                 v-for="(series,index) in latestSeries"
@@ -38,10 +52,25 @@
         </div>
 
         <div class="relative container mx-auto carousel">
-          <chevron :direction="directionLeft" :rotate="rotateLeft"/>
-          <chevron :direction="directionRight" :rotate="rotateRight"/>
-          <div
-              class="container mx-auto carousel bg-[white] rounded-xl outline outline-[1px] outline-[#dbdbdb] relative flex flex-nowrap  lg:overflow-hidden overflow-auto">
+
+          <chevron v-show="c2scroll > 1"
+                   id="c3"
+                   class="c3"
+                   @click.native="chevron"
+                   :direction="directionLeft"
+                   :rotate="rotateLeft"
+          />
+
+          <chevron v-show="c2scroll !== (c2max - current_width2)"
+                   id="c4"
+                   class="c4"
+                   @click.native="chevron"
+                   :direction="directionRight"
+                   :rotate="rotateRight"
+          />
+
+          <div id="carousel2"
+               class="container mx-auto carousel minh bg-[white] rounded-xl outline outline-[1px] outline-[#dbdbdb] relative flex flex-nowrap  lg:overflow-hidden overflow-auto">
             <card
                 class="p-1"
                 v-for="(series, index) in popularSeries"
@@ -52,7 +81,6 @@
                 :hang-prop="series.title"
                 :rank-prop="index+1"
                 :key="index"
-                @hook:mounted="componentLoaded"
             />
           </div>
         </div>
@@ -69,45 +97,97 @@ export default {
   layout: "custom",
   data() {
     return {
+      years: (min, max) => [...Array(max - min + 1).keys()].map(i => i + min),
       state: false,
       rotateRight: "rotate(180, 19.5, 19.5)",
       rotateLeft: "",
-      directionRight: "absolute z-[15] -right-[18px] top-[calc(50%_-_18px)]",
-      directionLeft: "absolute z-[15] -left-[18px] top-[calc(50%_-_18px)]",
-      years: (min, max) => [...Array(max - min + 1).keys()].map(i => i + min),
+      directionRight: "absolute z-[15] -right-[18px] top-[calc(50%_-_19px)]",
+      directionLeft: "absolute z-[15] -left-[18px] top-[calc(50%_-_19px)]",
       latestSeries: [],
       popularSeries: [],
-      isComponentLoaded: false
+      c1scroll: 0,
+      c2scroll: 0,
+      current_width: 0,
+      current_width2: 0,
+      c1max: 0,
+      c2max: 0
     }
   },
   methods: {
-    componentLoaded(){
-      this.isComponentLoaded = true
-      console.log(this.isComponentLoaded)
-    },
+
     apiCall(year) {
+
 
       axios.get(`/api/populars?q=${year}`)
           .catch(err => console.log(err))
           .then(res => {
             year.toString() === "2022" ? this.latestSeries = res.data : this.popularSeries = res.data
           })
-    }
+          .finally(r => {
+            this.c1max = document.getElementById("carousel1").scrollWidth
+            this.c2max = document.getElementById("carousel2").scrollWidth
+            year !== "2022" ? document.getElementById("carousel2").scroll({left: 0, behavior: "smooth"}) : null
+          })
+    },
+    chevron(e) {
+      const chevronClass = e.currentTarget.attributes[0].value
+      const currentWidth = document.querySelector("#carousel1").offsetWidth
+      const currentWidth2 = document.querySelector("#carousel2").offsetWidth
+      const carousel1 = document.getElementById("carousel1")
+      const carousel2 = document.getElementById("carousel2")
+
+
+      if (chevronClass === "c1") {
+        carousel1.scrollLeft -= currentWidth
+        carousel1.addEventListener("scroll", () => {
+          this.c1scroll = carousel1.scrollLeft
+        })
+      } else if (chevronClass === "c2") {
+        // carousel1.scroll({left: carousel1.scrollLeft + currentWidth, behavior: "smooth"})
+        carousel1.scrollLeft += currentWidth
+        carousel1.addEventListener("scroll", () => {
+          this.c1scroll = carousel1.scrollLeft
+        })
+      } else if (chevronClass === "c3") {
+        carousel2.scrollLeft += currentWidth
+        carousel2.addEventListener("scroll", () => {
+          this.c2scroll = carousel2.scrollLeft
+        })
+      } else if (chevronClass === "c4") {
+        carousel2.scrollLeft -= currentWidth
+        carousel2.addEventListener("scroll", () => {
+          this.c2scroll = carousel2.scrollLeft
+        })
+      }
+
+
+    },
+
+
+  },
+
+  beforeMount() {
+    this.apiCall(2022)
+    this.apiCall(2021)
+
   },
 
   mounted() {
-    this.apiCall(2022)
-    this.apiCall(2021)
-  }
+    this.current_width = document.getElementById("carousel1").scrollWidth
+    this.current_width2 = document.getElementById("carousel2").scrollWidth
+
+
+    const resizeObserver = new ResizeObserver(e => {
+      this.current_width = e[0].contentRect.width
+      this.c1max = document.getElementById("carousel1").scrollWidth
+      this.c2max = document.getElementById("carousel2").scrollWidth
+    })
+
+    resizeObserver.observe(document.getElementById("carousel1"))
+
+  },
+
 
 }
 </script>
 
-<style>
-
-.ubuntu-font {
-  font-family: "Ubuntu", serif;
-}
-
-
-</style>
