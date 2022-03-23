ae30a3b3ddfc7cd924522a73a79966c5e937a246
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-23 05:11:53 +0300

    fix

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-show="!isImageLoaded" class="lg:w-[180px] lg:h-[253px] w-[103px] h-[145px] bg-[#dbdbdb] rounded-xl relative">
+    <div v-show="!isImageLoaded" class="lg:w-[180px] lg:h-[253px] w-[103px] h-[145px] bg-[#dbdbdb] lg:rounded-xl rounded-[5px] relative">
       <div class="animate-pulse">
         <div class="lg:w-[60px] lg:h-[60px] w-[45px] h-[45px] bg-[#fafafa] absolute top-1 right-1 rounded-full"></div>
         <div class="w-full h-[60px] rounded-b-xl absolute bottom-0 grid grid-rows-3 place-items-center">
@@ -11,14 +11,14 @@
       </div>
     </div>
     <div v-show="isImageLoaded"
-         class="custom-card cursor-pointer lg:w-[180px] w-[103px] outline outline-[1px] outline-[#dbdbdb] rounded-xl relative overflow-hidden">
+         class="custom-card cursor-pointer lg:w-[180px] w-[103px] outline outline-[1px] outline-[#dbdbdb] lg:rounded-xl rounded-[5px] relative overflow-hidden">
       <div v-if="renderRank" class="absolute top-1 right-1 z-10 lg:w-[60px] w-[45px]">
         <img src="/templates/rankCircle.svg" alt="rankCircleSvg" class="w-full h-full">
         <div class="absolute md:w-full w-full text-center lg:top-3 top-2.5">
           <span class="text-[#ffc107] lg:text-2xl text-[18px]">#{{ rankProp }}</span></div>
       </div>
 
-      <div v-if="renderRate" class="absolute top-1 right-1 z-10 hidden">
+      <div v-if="renderRate" class="absolute top-1 right-1 z-10 ">
 
         <svg id="svgProgress" height="0" width="0" viewBox="0 0 100 100"
              class="lg:w-[60px] lg:h-[60px] w-[45px] h-[45px]">
@@ -40,20 +40,21 @@
               fill="transparent"
               stroke-width="6"
               :stroke-dasharray="2*Math.PI*40"
-              :stroke-dashoffset="2*Math.PI*40 - (2*Math.PI*45*(parseInt(renderRate)/100))"
+              :stroke-dashoffset="2*Math.PI*40 - (2*Math.PI*45*(rateProp/100))"
               transform="rotate(-90 50 50)"/>
           <text text-anchor="middle" x="50%" y="45%" fill="#ffc107" font-size="30px">{{ rateProp }}%</text>
           <text text-anchor="middle" x="50%" y="70%" fill="#ffc107" font-size="23px">Similar</text>
         </svg>
       </div>
 
-      <img @load="imageLoadStatus" :src="'/img/' + idProp + '.webp'" class="card-image object-fill w-full rounded-xl lg:w-[180px] lg:h-[253px] w-[103px] h-[145px]" alt="">
+      <img @load="imageLoadStatus" :src="'/img/' + idProp + '.webp'"
+           class="card-image object-fill w-full lg:rounded-xl rounded-[5px] lg:w-[180px] lg:h-[253px] w-[103px] h-[145px]" alt="">
       <div class="absolute bottom-0 z-10 text-center text-[red] w-full lg:text-sm text-[0.75rem]">
         <p class="font-semibold text-[#f5f5f5]">{{ nameProp }}</p>
         <p class="text-[#f5f5f5] lg:block hidden">{{ hangProp }}</p>
         <p class="font-semibold text-[#ffc107]">{{ yearProp }}</p>
       </div>
-      <div class="bg-gradient-to-t from-[black] w-full h-1/2 absolute bottom-0 rounded-b-xl"></div>
+      <div class="bg-gradient-to-t from-[black] w-full h-1/2 absolute bottom-0 lg:rounded-b-xl rounded-b-[5px]"></div>
     </div>
   </div>
 </template>
@@ -78,7 +79,7 @@ export default {
     }
   },
   methods: {
-    imageLoadStatus(){
+    imageLoadStatus() {
       this.isImageLoaded = true
     }
   }
@@ -87,9 +88,6 @@ export default {
 
 <style>
 /*103 138*/
-.card-image {
-  transition: transform .4s;
-}
 
 .custom-card:hover .card-image {
   transform: scale(1.1);
