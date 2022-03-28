c7924e2626e9771770365105ee5e08caad130d62
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-29 02:49:45 +0300

    sticky header

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <nav class="bg-white border-b border-[#DBDBDB] w-full">
+    <nav :class="'bg-white border-b border-[#DBDBDB] w-full' + (this.flag ? ' minimised icy' : ' not-minimised')">
       <div
-          class="container d:h-[74px] t:h-[74px] h-[90px] mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center">
+          :class="'container mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center' + (flag ? ' md:h-[55px] h-[50px]':' md:h-[74px] h-[90px]')">
         <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%] grid grid-col-1 items-center">
-          <a href="/" class="inline-block"><img src="/templates/bannerV3x64.png" alt="" class=""></a>
+          <img @click="$router.push('/')" src="/templates/bannerV3x64.png" alt="" :class="'cursor-pointer' + (flag ? ' hidden' : '')">
         </div>
-        <div class="t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0">
-          <search-box/>
+        <div :class="'t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0' + (flag ? ' mb-[5px] md:mb-[0]' : '')" >
+          <search-box :flag-prop="flag"/>
         </div>
-        <button id="testButton" class="bg-[yellow] rounded-xl w-[80px] border border-[black] absolute right-[15%]">Click</button>
       </div>
     </nav>
   </div>
@@ -17,7 +16,23 @@
 
 
 <script>
-export default {}
+export default {
+
+  data() {
+    return {
+      flag: false
+    }
+  },
+
+  mounted() {
+    window.addEventListener("scroll", e => {
+      this.flag = window.scrollY > 75
+    })
+  },
+
+
+
+}
 </script>
 
 
