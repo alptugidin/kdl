b87e3e1066e66dd5339f4b7f31ffdd7cb051a7c3
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-03-30 05:33:28 +0300

    banner href

--- components/Header.vue
+++ components/Header.vue
@@ -4,11 +4,12 @@
       <div
           :class="'container mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center' + (flag ? ' md:h-[55px] h-[50px]':' md:h-[74px] h-[90px]')">
         <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%] grid grid-col-1 items-center">
-          <img @click="$router.push('/')" src="/templates/bannerV3x64.png" alt="" :class="'cursor-pointer' + (flag ? ' hidden' : '')">
+          <img @click="goHome" src="/templates/bannerV3x64.png" alt="" :class="'cursor-pointer' + (flag ? ' hidden' : '')">
         </div>
         <div :class="'t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0' + (flag ? ' mb-[5px] md:mb-[0]' : '')" >
           <search-box :flag-prop="flag"/>
         </div>
+        <button id="logButton" class="w-[80px] bg-[#EFEFEF] border border-[#dbdbdb] cursor-pointer rounded-lg absolute right-[20%]">click</button>
       </div>
     </nav>
   </div>
@@ -17,7 +18,11 @@
 
 <script>
 export default {
-
+  methods:{
+    goHome(){
+      window.location.href = "/"
+    }
+  },
   data() {
     return {
       flag: false
