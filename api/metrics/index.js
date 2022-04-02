97cc3b81c2141037497bf986a6f5b8267a5aa1ea
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-04-02 05:43:11 +0300

    fs.appendfile fix

--- api/metrics/index.js
+++ api/metrics/index.js
@@ -6,9 +6,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.text())
 app.use((req, res) => {
     res.sendStatus(204)
-    // fs.appendFile("./log.txt", req.body + "\n", e => {
-    //     console.log("hata " + e)
-    // })
+    fs.appendFile("./log.txt", req.body + "\n", e => e)
 });
 
 
