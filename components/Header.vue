9a0d1be6564502b43640024a537a7dc0f5295a17
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-04-04 05:36:28 +0300

    fix

--- components/Header.vue
+++ components/Header.vue
@@ -4,9 +4,10 @@
       <div
           :class="'container mx-auto grid d:grid-cols-3 t:grid-cols-4 grid-cols-1 items-center' + (flag ? ' md:h-[55px] h-[50px]':' md:h-[74px] h-[90px]')">
         <div class="d:w-auto t:w-3/4 w-1/4 d:ml-0 t:ml-0 ml-[37.5%] grid grid-col-1 items-center">
-          <img @click="goHome" src="/templates/bannerV3x64.png" alt="" :class="'cursor-pointer' + (flag ? ' hidden' : '')">
+          <img @click="goHome" src="/templates/bannerV3x64.png" alt=""
+               :class="'cursor-pointer' + (flag ? ' hidden' : '')">
         </div>
-        <div :class="'t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0' + (flag ? ' mb-[5px] md:mb-[0]' : '')" >
+        <div :class="'t:col-start-2 t:col-span-2 px-1.5 d:px-0 t:px-0' + (flag ? ' mb-[5px] md:mb-[0]' : '')">
           <search-box :flag-prop="flag"/>
         </div>
       </div>
@@ -17,8 +18,8 @@
 
 <script>
 export default {
-  methods:{
-    goHome(){
+  methods: {
+    goHome() {
       window.location.href = "/"
     }
   },
@@ -33,7 +34,6 @@ export default {
       this.flag = window.scrollY > 75
     })
   },
-
 
 
 }
