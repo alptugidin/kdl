415095a842619d4f18fbe5ba6f794eaddb8ae5bb
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-04-04 05:36:40 +0300

    new watchlist component

---
+++ components/Watchlist.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="fixed right-5 bottom-0 z-20">
+      <div
+          class="flex absolute right-0 top-[-36px] w-[70px] h-[36px] bg-[white] border border-[#c4c4c4] border-b-0 rounded-tl-lg rounded-tr-lg  custom-shadow cursor-pointer hover:border-[#c44d65] hover:border-b-0 transition">
+        <div class="w-1/2 h-full py-2 pl-3">
+          <img src="/templates/fav.svg" alt="" class="">
+        </div>
+        <div class="w-1/2 h-full leading-[36px] text-center pr-3">
+          <span class="text-xl text-[#C7042C] font-semibold">1</span>
+        </div>
+      </div>
+      <div class="w-[300px] h-[360px] bg-[white] border border-[#dbdbdb] rounded-t-lg">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Watchlist"
+}
+</script>
+
+<style scoped>
+
+</style>
