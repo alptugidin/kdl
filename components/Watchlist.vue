9ce1537b9ac219ded7d01e03d3d6e8f3f0d23003
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-04-05 04:39:39 +0300

    watchlist try 1

--- components/Watchlist.vue
+++ components/Watchlist.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <div class="fixed right-5 bottom-0 z-20">
-      <div
-          class="flex absolute right-0 top-[-36px] w-[70px] h-[36px] bg-[white] border border-[#c4c4c4] border-b-0 rounded-tl-lg rounded-tr-lg  custom-shadow cursor-pointer hover:border-[#c44d65] hover:border-b-0 transition">
-        <div class="w-1/2 h-full py-2 pl-3">
+      <div @click="changeWatchlistState"
+           class="flex watchlist-head absolute left-[230px] top-[-36px] w-[70px] h-[36px] bg-[white] rounded-tl-lg rounded-tr-lg  custom-shadow cursor-pointer">
+        <div class="wd-hd-fc w-1/2 h-full py-2 pl-3">
           <img src="/templates/fav.svg" alt="" class="">
         </div>
-        <div class="w-1/2 h-full leading-[36px] text-center pr-3">
-          <span class="text-xl text-[#C7042C] font-semibold">1</span>
+        <div class="wd-hd-sc w-1/2 h-full leading-[36px] text-center pr-3">
+          <span id="wl-span" class="text-xl text-[#C7042C] font-semibold">1</span>
         </div>
+        <div id="min-chevron" class="p-1 absolute right-0 top-[3px] hidden">
+          <img src="/templates/minimize.svg" alt="">
+        </div>
+
       </div>
-      <div class="w-[300px] h-[360px] bg-[white] border border-[#dbdbdb] rounded-t-lg">
+      <div id="watchlist-ghost" class="w-full h-[6px] bg-[white] absolute border border-t-[#dbdbdb] border-b-0 border-x-0"></div>
+      <div
+          class="watchlist w-[300px] h-[360px] max-h-[360px] overflow-auto bg-[white] custom-shadow mb-[-360px] transition-all leading-[36px] ">
+        <ul id="watchlist-ul" class="">
+          <li v-for="i in 12" class="wl-li h-[36px] px-1 border border-b-[#dbdbdb] border-t-0 border-x-0 font-semibold ubuntu-font hover:bg-[#dbdbdb]">Korean Drama</li>
+        </ul>
       </div>
     </div>
 
@@ -19,10 +28,33 @@
 
 <script>
 export default {
-  name: "Watchlist"
+  name: "Watchlist",
+  data() {
+    return {
+      state: false,
+      watchlistLen: 1
+    }
+  },
+  methods: {
+    changeWatchlistState() {
+      document.querySelector(".watchlist").classList.toggle("wl-open")
+      document.querySelector(".watchlist-head").classList.toggle("wl-hd-open")
+      document.querySelector(".wd-hd-fc").classList.toggle("fc-open")
+      document.querySelector(".wd-hd-sc").classList.toggle("sc-open")
+      document.querySelector("#min-chevron").classList.toggle("hidden")
+
+      setTimeout(function () {
+        const value = document.getElementById("wl-span").innerText
+        if (value !== "Watchlist") {
+          document.getElementById("wl-span").innerText = "Watchlist"
+        } else {
+          document.getElementById("wl-span").innerText = 1
+        }
+      }, 50)
+
+
+    }
+  },
+
 }
 </script>
-
-<style scoped>
-
-</style>
