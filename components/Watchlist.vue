45fcff573824fd8d48190adb3122e58e216ce320
Alptuğ <31244930+alptugidin@users.noreply.github.com>
2022-04-05 05:30:30 +0300

    watchlist try 2

--- components/Watchlist.vue
+++ components/Watchlist.vue
@@ -1,60 +1,45 @@
 <template>
   <div>
-    <div class="fixed right-5 bottom-0 z-20">
-      <div @click="changeWatchlistState"
-           class="flex watchlist-head absolute left-[230px] top-[-36px] w-[70px] h-[36px] bg-[white] rounded-tl-lg rounded-tr-lg  custom-shadow cursor-pointer">
-        <div class="wd-hd-fc w-1/2 h-full py-2 pl-3">
-          <img src="/templates/fav.svg" alt="" class="">
+    <div
+        class="wl fixed w-[70px] h-[396px] bg-[white] rounded-t-lg bottom-0 right-5 z-20 custom-shadow mb-[-360px] transition-all">
+      <div @click="showList"
+           class="flex wl-head absolute top-0 h-[36px] w-full bg-[white] rounded-t-lg  cursor-pointer leading-[36px]">
+        <div class="wl-left w-1/2 h-[36px]">
+          <img src="/templates/fav.svg" alt="" class="wl-left-image mt-[8px] ml-[8px]">
         </div>
-        <div class="wd-hd-sc w-1/2 h-full leading-[36px] text-center pr-3">
-          <span id="wl-span" class="text-xl text-[#C7042C] font-semibold">1</span>
+        <div class="wl-right w-1/2 text-center">
+          <span id="wl-text" class="font-semibold ubuntu-font text-[#C71B2B] text-[20px]">2</span>
         </div>
-        <div id="min-chevron" class="p-1 absolute right-0 top-[3px] hidden">
-          <img src="/templates/minimize.svg" alt="">
-        </div>
-
       </div>
-      <div id="watchlist-ghost" class="w-full h-[6px] bg-[white] absolute border border-t-[#dbdbdb] border-b-0 border-x-0"></div>
-      <div
-          class="watchlist w-[300px] h-[360px] max-h-[360px] overflow-auto bg-[white] custom-shadow mb-[-360px] transition-all leading-[36px] ">
-        <ul id="watchlist-ul" class="">
-          <li v-for="i in 12" class="wl-li h-[36px] px-1 border border-b-[#dbdbdb] border-t-0 border-x-0 font-semibold ubuntu-font hover:bg-[#dbdbdb]">Korean Drama</li>
-        </ul>
+      <div class="wl-body">
+
       </div>
     </div>
-
   </div>
 </template>
-
 <script>
 export default {
-  name: "Watchlist",
   data() {
-    return {
-      state: false,
-      watchlistLen: 1
-    }
+    return {}
   },
   methods: {
-    changeWatchlistState() {
-      document.querySelector(".watchlist").classList.toggle("wl-open")
-      document.querySelector(".watchlist-head").classList.toggle("wl-hd-open")
-      document.querySelector(".wd-hd-fc").classList.toggle("fc-open")
-      document.querySelector(".wd-hd-sc").classList.toggle("sc-open")
-      document.querySelector("#min-chevron").classList.toggle("hidden")
-
-      setTimeout(function () {
-        const value = document.getElementById("wl-span").innerText
-        if (value !== "Watchlist") {
-          document.getElementById("wl-span").innerText = "Watchlist"
-        } else {
-          document.getElementById("wl-span").innerText = 1
-        }
-      }, 50)
-
-
+    showList() {
+      document.getElementById("wl-text").classList.toggle("hidden")
+      setTimeout(() => {
+        document.getElementById("wl-text").classList.toggle("hidden")
+      }, 150)
+      document.querySelector(".wl").classList.toggle("wl-up")
+      document.querySelector(".wl-head").classList.toggle("bg-[#efefef]")
+      document.querySelector(".wl-left").classList.toggle("w-2/12")
+      document.querySelector(".wl-left-image").classList.toggle("ml-[calc(calc(100%_-_16px)/2)]")
+      document.querySelector(".wl-right").classList.toggle("w-10/12")
+      document.querySelector(".wl-right").classList.toggle("text-center")
+      if (document.getElementById("wl-text").innerText !== "Watchlist") {
+        document.getElementById("wl-text").innerText = "Watchlist"
+      } else {
+        document.getElementById("wl-text").innerText = "2"
+      }
     }
-  },
-
+  }
 }
 </script>
